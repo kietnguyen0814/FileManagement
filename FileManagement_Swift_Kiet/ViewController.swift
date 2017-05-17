fd21c729ff28874aab69e515cb34b71a43e24dc6
Nguyễn Anh Kiệt <kietnguyen0814@gmail.com>
2017-05-17 19:27:51 +0700

    Initial Commit

---
+++ FileManagement_Swift_Kiet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FileManagement_Swift_Kiet
+//
+//  Created by Kiet Nguyen on 5/17/17.
+//  Copyright © 2017 Kiet Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
